52178b9cd90ecdf1662d38669b14919ed7a6bed7
momomo546 <108210454+momomo546@users.noreply.github.com>
2023-03-19 00:40:35 +0900

    stage

---
+++ muse/Assets/Script/Floor_loop.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor_loop : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(Time.deltaTime * speed,0,0);
+
+        if(transform.position.x <= -30f)
+        {
+            transform.position = new Vector3(30,0,0);
+        }
+    }
+}
