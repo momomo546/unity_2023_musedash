f41aa252fe611873e05906e443443aa79f4147d5
momomo546 <108210454+momomo546@users.noreply.github.com>
2023-03-26 00:15:00 +0900

    main

---
+++ muse/Assets/Script/setting.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setting : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float bpm = 130f;
+}
