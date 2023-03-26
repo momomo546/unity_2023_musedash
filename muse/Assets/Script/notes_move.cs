2005f026455052fd10ea2c0623047e08a1433824
momomo546 <108210454+momomo546@users.noreply.github.com>
2023-03-26 15:23:56 +0900

    main

---
+++ muse/Assets/Script/notes_move.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class notes_move : MonoBehaviour
+{
+    public setting Setting;
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Setting.speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(Time.deltaTime * speed,0,0);
+
+        if(transform.position.x <= -6f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
